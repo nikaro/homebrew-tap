2682a0b67ed58c51aac1e3dc35cd8a8d12d73aaa
Nicolas Karolak <nicolas@karolak.fr>
2021-02-14 11:15:20 +0100

    resume-cli 3.0.4 (new formula)

---
+++ Formula/resume-cli.rb
@@ -0,0 +1,20 @@
+require "language/node"
+
+class ResumeCli < Formula
+  desc "CLI tool to easily setup a new resume 📑"
+  homepage "https://jsonresume.org"
+  url "https://registry.npmjs.org/resume-cli/-/resume-cli-3.0.4.tgz"
+  sha256 "1d139dc539e05f176072e4a449e38b8049e216d3aefa940d08e77913a3cd2fc9"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/resume", "--version"
+  end
+end
