68691442d6aae19a65053ac64db252a742fba6ba
Nicolas Karolak <nicolas@karolak.fr>
2021-06-01 19:20:24 +0200

    resume-pycli 1.3.0
    Closes #11.
    
    Signed-off-by: Nicolas Karolak <3918653+nikaro@users.noreply.github.com>

--- Formula/resume-pycli.rb
+++ Formula/resume-pycli.rb
@@ -3,8 +3,8 @@ class ResumePycli < Formula
 
   desc "CLI tool to easily setup a new resume"
   homepage "https://sr.ht/~nka/resume-pycli"
-  url "https://files.pythonhosted.org/packages/12/75/d4f2a5323538b50dc4c9ff56578e02001159b858b565e88570e9d7d5f0fb/resume-pycli-1.2.0.tar.gz"
-  sha256 "4de76b08bbe1ee6d6f8a00070db44d2d3352a833750f387e87c2b9dcfc57f724"
+  url "https://files.pythonhosted.org/packages/75/7e/d844c55f585b188416e6c7ed887f206681c5e22025a0554c565923408b09/resume-pycli-1.3.0.tar.gz"
+  sha256 "eaf6747ea4b81c82f9344bbeeddb0b9694d7924f004d8e69fc0ff27304ef4d28"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -20,13 +20,13 @@ class ResumePycli < Formula
   end
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/d5/99/286fd2fdfb501620a9341319ba47444040c7b3094d3b6c797d7281469bf8/click-8.0.0.tar.gz"
-    sha256 "7d8c289ee437bcb0316820ccee14aefcb056e58d31830ecab8e47eda6540e136"
+    url "https://files.pythonhosted.org/packages/21/83/308a74ca1104fe1e3197d31693a7a2db67c2d4e668f20f43a2fca491f9f7/click-8.0.1.tar.gz"
+    sha256 "8c04c11192119b1ef78ea049e0a6f0463e4c48ef00a30160c704337586f3ad7a"
   end
 
   resource "Jinja2" do
-    url "https://files.pythonhosted.org/packages/7a/0c/23cbcf515b5394e9f59a3e6629f26e1142b92d474ee0725a26aa5a3bcf76/Jinja2-3.0.0.tar.gz"
-    sha256 "ea8d7dd814ce9df6de6a761ec7f1cac98afe305b8cdc4aaae4e114b8d8ce24c5"
+    url "https://files.pythonhosted.org/packages/39/11/8076571afd97303dfeb6e466f27187ca4970918d4b36d5326725514d3ed3/Jinja2-3.0.1.tar.gz"
+    sha256 "703f484b47a6af502e743c9122595cc812b0271f661722403114f71a79d0f5a4"
   end
 
   resource "jsonschema" do
@@ -35,8 +35,8 @@ class ResumePycli < Formula
   end
 
   resource "MarkupSafe" do
-    url "https://files.pythonhosted.org/packages/67/6a/5b3ed5c122e20c33d2562df06faf895a6b91b0a6b96a4626440ffe1d5c8e/MarkupSafe-2.0.0.tar.gz"
-    sha256 "4fae0677f712ee090721d8b17f412f1cbceefbf0dc180fe91bab3232f38b4527"
+    url "https://files.pythonhosted.org/packages/bf/10/ff66fea6d1788c458663a84d88787bae15d45daa16f6b3ef33322a51fc7e/MarkupSafe-2.0.1.tar.gz"
+    sha256 "594c67807fb16238b30c44bdf74f36c02cdf22d1c8cda91ef8a0ed8dabf5620a"
   end
 
   resource "pdfkit" do
