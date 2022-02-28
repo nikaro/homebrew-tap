599d2a9ae636a92ba6c5f8c88becfbedb716142d
Nicolas Karolak <nicolas@karolak.fr>
2022-02-28 14:55:35 +0100

    update url

--- Casks/instantview.rb
+++ Casks/instantview.rb
@@ -5,10 +5,10 @@ cask "instantview" do
   url "https://www.siliconmotion.com/downloads/macOS_InstantView_#{version}.dmg"
   name "InstantView"
   desc "Driver for Silicon Motion's SM76x USB display/docking station SoC"
-  homepage "https://www.siliconmotion.com/download/3Xf/a/index.html"
+  homepage "https://www.siliconmotion.com/downloads/index.html"
 
   livecheck do
-    url "https://www.siliconmotion.com/download/3Xf/a/index.html"
+    url "https://www.siliconmotion.com/downloads/index.html"
     strategy :page_match
     regex(%r{href=".*/macOS_InstantView_(V\d+\.\d+R\d+)\.dmg"}i)
   end
