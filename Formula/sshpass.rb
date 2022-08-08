fb8dccd1e248e0011edba165d918702f6e8391b0
Nicolas Karolak <nicolas@karolak.fr>
2022-08-08 14:46:49 +0200

    fix url

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,7 +1,7 @@
 class Sshpass < Formula
   desc "Tool for non-interactivly performing password authentication with SSH"
   homepage "https://sourceforge.net/projects/sshpass/"
-  url "https://sourceforge.net/projects/sshpass/files/sshpass/1.09/sshpass-1.09.tar.gz"
+  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.09/sshpass-1.09.tar.gz"
   sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
   license "GPL-2.0-or-later"
 
