a6d47102ba8b83afe9783f9096c5f0d656f54ff3
Nicolas Karolak <nicolas@karolak.fr>
2022-12-20 14:24:29 +0100

    update marksman to 2022-12-19

--- Formula/marksman.rb
+++ Formula/marksman.rb
@@ -2,11 +2,11 @@ class Marksman < Formula
   desc "Language Server Protocol for Markdown"
   homepage "https://github.com/artempyanykh/marksman"
   license "MIT"
-  version "2022-11-25"
+  version "2022-12-19"
 
   on_macos do
     url "https://github.com/artempyanykh/marksman/releases/download/2022-11-25/marksman-macos"
-    sha256 "99c23f5091a9e1eee7577c71caa18c48800d10fc0c737d1a56680bc7e271dae7"
+    sha256 "fe3de5bce0caab3f54619fb795f749e23f4c0555c0a474f1d7aa0047f7eb68eb"
     def install
       bin.install "marksman-macos" => "marksman"
     end
@@ -14,7 +14,7 @@ class Marksman < Formula
 
   on_linux do
     url "https://github.com/artempyanykh/marksman/releases/download/2022-11-25/marksman-linux"
-    sha256 "64c5216a3339c9d1e26eff0722d7679dcbf61d4c1703ed05d1220a5ad99eb0fb"
+    sha256 "b559118400f174ad48c6a1aae2bebbbe2171d346c38476bbbeb5ee296465ed37"
     def install
       bin.install "marksman-macos" => "marksman"
     end
