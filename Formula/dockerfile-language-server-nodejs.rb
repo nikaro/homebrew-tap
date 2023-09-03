4862581d50cfa07a69549a269a370279834cc3fe
Nicolas Karolak <nicolas@karolak.fr>
2023-09-03 18:52:24 +0200

    update docker-langserver

--- Formula/dockerfile-language-server-nodejs.rb
+++ Formula/dockerfile-language-server-nodejs.rb
@@ -3,8 +3,8 @@ require "language/node"
 class DockerfileLanguageServerNodejs < Formula
   desc "This is a language server for Dockerfiles"
   homepage "https://www.npmjs.com/package/dockerfile-language-server-nodejs"
-  url "https://registry.npmjs.org/dockerfile-language-server-nodejs/-/dockerfile-language-server-nodejs-0.9.0.tgz"
-  sha256 "88a6085ca049ebdf383f3644ee699e982379e3767918bf14baedd0ef90c1e18f"
+  url "https://registry.npmjs.org/dockerfile-language-server-nodejs/-/dockerfile-language-server-nodejs-0.10.2.tgz"
+  sha256 "1509a3d44e795449bdf482c32da720a86de9d7d5ae9b2513209ce4503ede35e0"
   license "MIT"
 
   depends_on "node"
