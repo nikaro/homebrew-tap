50329eb3c73f279789f1eef6b73c6e3722a23433
Nicolas Karolak <nicolas@karolak.fr>
2023-09-04 11:35:20 +0200

    try something

--- Formula/docker-langserver.rb
+++ Formula/docker-langserver.rb
@@ -2,7 +2,7 @@ require "language/node"
 
 class DockerLangserver < Formula
   desc "This is a language server for Dockerfiles"
-  homepage "https://www.npmjs.com/package/dockerfile-language-server-nodejs"
+  homepage "https://github.com/rcjsuen/dockerfile-language-server-nodejs"
   url "https://registry.npmjs.org/dockerfile-language-server-nodejs/-/dockerfile-language-server-nodejs-0.10.2.tgz"
   sha256 "1509a3d44e795449bdf482c32da720a86de9d7d5ae9b2513209ce4503ede35e0"
   license "MIT"
@@ -12,5 +12,18 @@ class DockerLangserver < Formula
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    require "open3"
+
+    dockerfile = <<~DOCKERFILE
+      FROM debian:latest
+      RUN apt-get update
+    DOCKERFILE
+
+    Open3.popen3("#{bin}/docker-langserver", "--stdio") do |stdin, _stdout|
+      stdin.write to_s(dockerfile)
+    end
   end
 end
