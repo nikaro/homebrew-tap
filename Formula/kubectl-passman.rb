3c56947ef73a68b233c43bf8f137dff7c2d3e1e7
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2023-11-16 09:46:46 +0000

    kubectl-passman: add 1.2.3 bottle.

--- Formula/kubectl-passman.rb
+++ Formula/kubectl-passman.rb
@@ -5,6 +5,12 @@ class KubectlPassman < Formula
   sha256 "6da71353a39775e3a43788a9cc8cdbacf3204ec3ca52fa098fefa5d3963c10b7"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/kubectl-passman-1.2.3"
+    sha256 cellar: :any_skip_relocation, ventura:      "4b5e0e8d486290fccc80597cfb4a964e6f753ff63b41019d3cd197985e5887c9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f2a1cf3b344bf0492bb953828a1b5ff936ad187e8bff4b329af31525cd73623e"
+  end
+
   depends_on "go" => :build
 
   def install
