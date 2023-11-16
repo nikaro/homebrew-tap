5702a0f7d632b06c08aa3b3e46e6df88cd133d4a
Nicolas Karolak <nicolas@karolak.fr>
2023-11-16 09:24:22 +0100

    feat(kubectl-passman): update to v1.2.3

--- Formula/kubectl-passman.rb
+++ Formula/kubectl-passman.rb
@@ -5,23 +5,23 @@ class KubectlPassman < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/chrisns/kubectl-passman/releases/download/v1.2.2/kubectl-passman-darwin-amd64.zip"
-      sha256 "5491e46cd9c2398f5046358088a819c0813a396a738e3edf9264e95bb8040101"
+      url "https://github.com/chrisns/kubectl-passman/releases/download/v1.2.3/kubectl-passman-darwin-amd64.zip"
+      sha256 "d6d9138af60ec1a57645dac8e4ce93dadd84efe1802543e05bff45871dcec2e6"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/chrisns/kubectl-passman/releases/download/v1.2.2/kubectl-passman-darwin-arm64.zip"
-      sha256 "430f46c3b231494269a9a33e73b7ffe13505817344168cd78bb3d9808c4576af"
+      url "https://github.com/chrisns/kubectl-passman/releases/download/v1.2.3/kubectl-passman-darwin-arm64.zip"
+      sha256 "762bb02a15ba2ef7029a38a1ca7860a861c2f4bda4b241d32bf417aef7a57c29"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/chrisns/kubectl-passman/releases/download/v1.2.2/kubectl-passman-linux-amd64.zip"
-      sha256 "62c3f764fd7950c2ba2e2b3898a4d729a8be3ea6685e31258a8e3e5274b3568d"
+      url "https://github.com/chrisns/kubectl-passman/releases/download/v1.2.3/kubectl-passman-linux-amd64.zip"
+      sha256 "dac6b4eb52ffae5c0b230144dd9fe1e4f573c5b34524edac4ff82e30b46c7c0a"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/chrisns/kubectl-passman/releases/download/v1.2.2/kubectl-passman-linux-arm64.zip"
-      sha256 "4f9f6f0484199469e1fa70224fff32bc986bc7637f1bf9dfb866b14e5c7ccfc8"
+      url "https://github.com/chrisns/kubectl-passman/releases/download/v1.2.3/kubectl-passman-linux-arm64.zip"
+      sha256 "e5a0c2f3b0b66b0cb0a078420ecfd102a3f12ef63a05fc89d32d0e473c987c19"
     end
   end
 
