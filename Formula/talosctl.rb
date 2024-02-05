f11a5783db433d9889a8cd1946fbcbec7f63321c
Nicolas Karolak <nicolas@karolak.fr>
2024-02-05 12:10:14 +0100

    fix(deps): update homebrew formula siderolabs/talos to v1.6.4

--- Formula/talosctl.rb
+++ Formula/talosctl.rb
@@ -1,21 +1,21 @@
 class Talosctl < Formula
   desc "Talos Linux is a modern Linux distribution built for Kubernetes"
-  homepage "https://github.con/siderolabs/talos"
-  version "1.6.3"
+  homepage "https://github.com/siderolabs/talos"
+  version "1.6.4"
   license "MPL-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/siderolabs/talos/releases/download/v1.6.3/talosctl-darwin-arm64"
-      sha256 "fa7a697fe2b1d2043dbb69218de758969f068f09a69c1525f0f7fcc8a003fdcb"
+      url "https://github.com/siderolabs/talos/releases/download/v1.6.4/talosctl-darwin-arm64"
+      sha256 "3d83092c651d0926fa2d4840a540d82a1904519c89672ebeee2db1844f9620db"
 
       def install
         bin.install "talosctl-darwin-arm64" => "talosctl"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/siderolabs/talos/releases/download/v1.6.3/talosctl-darwin-amd64"
-      sha256 "f86948ead49f3202f8a50db5f02eb2a5315fab46626379f980a3da0bdaab91c2"
+      url "https://github.com/siderolabs/talos/releases/download/v1.6.4/talosctl-darwin-amd64"
+      sha256 "8e8a8271e4c3449fb2f845f8ae6a3ef153a006c5b08a4545b060e07ee10e986d"
 
       def install
         bin.install "talosctl-darwin-amd64" => "talosctl"
@@ -25,16 +25,16 @@ class Talosctl < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/siderolabs/talos/releases/download/v1.6.3/talosctl-linux-amd64"
-      sha256 "e2296700b8101fb91e554290e31ddcb1c2f9e2e5f33d31533b8f6bbb68c4e3ba"
+      url "https://github.com/siderolabs/talos/releases/download/v1.6.4/talosctl-linux-amd64"
+      sha256 "9346ae645f8dbcf1d63601c1dbf725d9e0475b225a9c483de872912fb9d87a01"
 
       def install
         bin.install "talosctl-linux-amd64" => "talosctl"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/siderolabs/talos/releases/download/v1.6.3/talosctl-linux-arm64"
-      sha256 "d841cbf4d4a3f5e80e0f9be2a7d75cf90bb2a4cb4129df0e3f5c4e204748331a"
+      url "https://github.com/siderolabs/talos/releases/download/v1.6.4/talosctl-linux-arm64"
+      sha256 "b1085ca8f86658532cadca71d77a013e203a60a6b16d3726a133e87ba4113ae1"
 
       def install
         bin.install "talosctl-linux-arm64" => "talosctl"
