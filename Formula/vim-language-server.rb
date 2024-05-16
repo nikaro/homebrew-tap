aaa0cb2227a4fbad597589bd7e84c79c831f7bae
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2024-05-16 21:30:43 +0000

    vim-language-server: update 2.3.1_1 bottle.

--- Formula/vim-language-server.rb
+++ Formula/vim-language-server.rb
@@ -10,9 +10,9 @@ class VimLanguageServer < Formula
   head "https://github.com/iamcco/vim-language-server.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/nikaro/homebrew-tap/releases/download/vim-language-server-2.3.1"
-    sha256 cellar: :any_skip_relocation, ventura:      "2816ecb968df9ce54e15371311f79f58500648479aa55d9991f1d984be56022e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "8481b18cb9054d168a81913ce9c698946a65b3853e6bbe4208cb362e7442553b"
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/vim-language-server-2.3.1_1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "40cadc75c474ad716f6d7f87879ca3b0d1d85861e0f803386a7288f9428db763"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "38cd726fdadef3ec52f38392fa900c22371f17bd77217aa824f19fd3d2c34d60"
   end
 
   depends_on "node"
