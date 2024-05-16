0239aa5f26902db0e107536ea64b58e108758b39
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2024-05-16 20:43:14 +0000

    vim-language-server: add 2.3.1 bottle.

--- Formula/vim-language-server.rb
+++ Formula/vim-language-server.rb
@@ -8,6 +8,12 @@ class VimLanguageServer < Formula
   license "MIT"
   head "https://github.com/iamcco/vim-language-server.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/vim-language-server-2.3.1"
+    sha256 cellar: :any_skip_relocation, ventura:      "2816ecb968df9ce54e15371311f79f58500648479aa55d9991f1d984be56022e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8481b18cb9054d168a81913ce9c698946a65b3853e6bbe4208cb362e7442553b"
+  end
+
   depends_on "node"
 
   def install
