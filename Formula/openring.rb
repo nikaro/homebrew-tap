51ddee95c2e9fc106b3f212aaa8cecd4544110e5
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2024-05-16 21:19:01 +0000

    openring: add 1.0.1_1 bottle.

--- Formula/openring.rb
+++ Formula/openring.rb
@@ -7,6 +7,12 @@ class Openring < Formula
   license "GPL-3.0-only"
   revision 1
 
+  bottle do
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/openring-1.0.1_1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "9a11695d614029a36b621e6ae512fdd56bad216db8c6f7a96819f037fd450878"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3c1495b9889bff9a34dafe7553031c6499346da8d2f29f2918e348e8070df4ee"
+  end
+
   depends_on "go" => :build
 
   def install
