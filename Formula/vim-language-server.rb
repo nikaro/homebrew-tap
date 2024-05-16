5edf474317b73daa2d5fb6e09c784dbf6f900dcc
Nicolas Karolak <nicolas@karolak.fr>
2024-05-16 21:32:28 +0200

    fix: archive url and sum

--- Formula/vim-language-server.rb
+++ Formula/vim-language-server.rb
@@ -3,8 +3,8 @@ require "language/node"
 class VimLanguageServer < Formula
   desc "VImScript language server, LSP for vim script"
   homepage "https://github.com/iamcco/vim-language-server"
-  url "https://registry.npmjs.org/vim-language-server/-/vim-language-server-3.2.5.tgz"
-  sha256 "c8d3eae160a892e32837db3dcae515e843e5383fef52b8141940c8bcf8b6d59f"
+  url "https://registry.npmjs.org/vim-language-server/-/vim-language-server-2.3.1.tgz"
+  sha256 "ffe0d18258a4b09bec46ec853f8838748c007c62c1fcf12d1eefedfaf19e1c46"
   license "MIT"
   head "https://github.com/iamcco/vim-language-server", branch: "master"
 
