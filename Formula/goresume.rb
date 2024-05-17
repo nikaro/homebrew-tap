a46eeeabc27d0a8097d61ce106c0e7e099b77a17
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2024-05-17 15:37:33 +0000

    goresume: add 0.3.5_1 bottle.

--- Formula/goresume.rb
+++ Formula/goresume.rb
@@ -6,6 +6,12 @@ class Goresume < Formula
   license "MIT"
   revision 1
 
+  bottle do
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/goresume-0.3.5_1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "4dd2dff63f3bfa9fb6633b0b1ff5fdf3b3ec9f88d8a0d908149721b30a9ca301"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "271b9821db5fe2384bcfebae56097d938e4ea7a2b391538a12d343bbc60dd6e8"
+  end
+
   depends_on "go" => :build
 
   def install
