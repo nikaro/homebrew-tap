1127586b221bc78357c1428ca396977ee7492171
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2024-05-17 16:05:49 +0000

    zabbixmon: add 0.9.3_1 bottle.

--- Formula/zabbixmon.rb
+++ Formula/zabbixmon.rb
@@ -6,6 +6,12 @@ class Zabbixmon < Formula
   license "GPL-3.0-or-later"
   revision 1
 
+  bottle do
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/zabbixmon-0.9.3_1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "445a152fe99145c7c74add9bf0c86a04585eeedba441e9eec0958a20f69ce1db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2f43767d1a62b32ac621bcaa86a3907bf160dd3aac32227545b001dcb6f50852"
+  end
+
   depends_on "go" => :build
 
   def install
