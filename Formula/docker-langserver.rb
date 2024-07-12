6cbae25cd08df8744ff1368d8b9725bd46116bf2
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2024-07-12 07:44:20 +0000

    docker-langserver: add 0.13.0 bottle.

--- Formula/docker-langserver.rb
+++ Formula/docker-langserver.rb
@@ -7,6 +7,12 @@ class DockerLangserver < Formula
   sha256 "8b471fe5bdbe2cc2f1eb9063b5817ee5500fea3e4bbb57d677a51634b6176c79"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/docker-langserver-0.13.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "f7a36db977d3bac08512443dd3ef625046c9e1d1af9566a9c5096e56157be929"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b64d9ed26bbd9e539ca23eedd89a15c9d2d9e5bc2f3727b8ab321abef1202460"
+  end
+
   depends_on "node"
 
   def install
