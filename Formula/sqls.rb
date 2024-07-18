41c01036042b0f9a370453ee5f763939f718cfec
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2024-07-18 12:26:26 +0000

    sqls: add 0.2.27 bottle.

--- Formula/sqls.rb
+++ Formula/sqls.rb
@@ -5,6 +5,12 @@ class Sqls < Formula
   sha256 "f47aff522ce20eb4f87fb099d492c35503824b10d40cb3d4b755bc0a8d6ddf56"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/sqls-0.2.27"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "6b3aa8781bc80f031f5e8ea65c330b39c6a9db04ccf7600edff12a96163a07a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2bda08d9011ab6b0fc2fae44ee0520149f4d5d221c6c559d7a6ad2a276f3dea1"
+  end
+
   depends_on "go" => :build
 
   def install
