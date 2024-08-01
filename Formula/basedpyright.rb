611ce03099cef5d972c1357308b723e3aadd80ec
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2024-08-01 06:58:04 +0000

    basedpyright: add 1.15.1 bottle.

--- Formula/basedpyright.rb
+++ Formula/basedpyright.rb
@@ -7,6 +7,12 @@ class Basedpyright < Formula
   sha256 "51fb7cfc0ad1eae6073c4b6360466f7f696b8b25b5779aee20589a4e01d3dd22"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/basedpyright-1.15.1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "4c89a246d46f47cbda0ead165c15ab074c729d997d34885476f2132fc7316878"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "161b32f442fb5c50e63b39f8753fb4e1c76258f96dfe25e178f8cdbf2bb34f93"
+  end
+
   depends_on "node"
 
   def install
