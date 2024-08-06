a5cea71713e022a0d65a23c52183d5b6a77ca9f7
Nicolas Karolak <nicolas@karolak.fr>
2024-08-06 11:15:25 +0200

    ci: replace makefile by taskfile

--- Formula/vim-language-server.rb
+++ Formula/vim-language-server.rb
@@ -17,7 +17,7 @@ class VimLanguageServer < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
