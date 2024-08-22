9b7374517a45b13e562d16ea76f670ebe1531499
Nicolas Karolak <nicolas@karolak.fr>
2024-08-22 19:13:55 +0200

    DeskTime 6.0.3 (new cask)

---
+++ Casks/desktime.rb
@@ -0,0 +1,27 @@
+cask "desktime" do
+  url_param = on_arch_conditional arm: "?m1", intel: ""
+
+  version "6.0.3"
+  sha256 arm:   "5dcb5b3ad275ed6bdf3b5b10be10e1990978312df384caf953cd03b2ad9fa2fa",
+         intel: "3d4fc653c6c302312748d60c5b7007a02354d00b1f562497b95ffdf3c9cd1b84"
+
+  url "https://desktime.com/updates/electron/mac#{url_param}"
+  name "DeskTime"
+  desc "Time tracker with additional workforce management features"
+  homepage "https://desktime.com/"
+
+  livecheck do
+    url :url
+    strategy :header_match
+  end
+
+  app "DeskTime.app"
+
+  uninstall quit: "DeskTime"
+
+  zap trash: [
+    "~/Library/Application Support/DeskTime",
+    "~/Library/Logs/DeskTime",
+    "~/Library/Preferences/com.desktime.DeskTime.plist",
+  ]
+end
