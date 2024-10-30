2de698baf13aafba6f62e462e2e45a9f08d46e1d
Nicolas Karolak <nikaro@users.noreply.github.com>
2024-10-30 09:37:34 +0100

    desktime 6.0.4 (#96)

--- Casks/desktime.rb
+++ Casks/desktime.rb
@@ -1,11 +1,9 @@
-# typed: false
-
 cask "desktime" do
   url_param = on_arch_conditional arm: "?m1", intel: ""
 
-  version "6.0.3"
-  sha256 arm:   "5dcb5b3ad275ed6bdf3b5b10be10e1990978312df384caf953cd03b2ad9fa2fa",
-         intel: "3d4fc653c6c302312748d60c5b7007a02354d00b1f562497b95ffdf3c9cd1b84"
+  version "6.0.4"
+  sha256 arm:   "c8049809c068ec433f9d8912af889f36482de681a57e716d71133d1f032db51a",
+         intel: "4281831da6db1eaab32848809a69dbfdc14113eb5de1b3080888576a7a56db0e"
 
   url "https://desktime.com/updates/electron/mac#{url_param}"
   name "DeskTime"
@@ -15,7 +13,7 @@ cask "desktime" do
   livecheck do
     url :url
     strategy :header_match
-    regex /filename=DeskTime-(\d+(?:\.\d+)+)-(?:x64|arm64)\.dmg/
+    regex(/filename=DeskTime-(\d+(?:\.\d+)+)-(?:x64|arm64)\.dmg/)
   end
 
   app "DeskTime.app"
