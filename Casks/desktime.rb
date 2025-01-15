90be934cae34325237069ff64944fc4a921e3c23
Nicolas Karolak <nikaro@users.noreply.github.com>
2025-01-15 14:35:36 +0100

    desktime 6.0.7 (#107)
    * desktime 6.0.7

--- Casks/desktime.rb
+++ Casks/desktime.rb
@@ -3,9 +3,9 @@
 cask "desktime" do
   url_param = on_arch_conditional arm: "?m1", intel: ""
 
-  version "6.0.6"
-  sha256 arm:   "2378166310543ec29ff4b1699bc44ad373e619a34274849f9915052f7b0714c0",
-         intel: "0a17e5c5b3260b4800b1dc92e815f835be81a56f782bbbd9e0e93fe75761d614"
+  version "6.0.7"
+  sha256 arm:   "b199ad26a43d8f287938f4b77d38e2834de122d4a120885b7ae29d6ca0016884",
+         intel: "ad7e767d5ea52396f6801e5473724ef79958523db163f76a5993bb622e198fd7"
 
   url "https://desktime.com/updates/electron/mac#{url_param}"
   name "DeskTime"
