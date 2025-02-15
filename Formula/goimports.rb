98608229dd2355b79b10f7bf6aefea9096b863c8
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2025-02-15 16:42:09 +0000

    goimports: update 0.30.0 bottle.

--- Formula/goimports.rb
+++ Formula/goimports.rb
@@ -8,9 +8,9 @@ class Goimports < Formula
   license "BSD-3-Clause"
 
   bottle do
-    root_url "https://github.com/nikaro/homebrew-tap/releases/download/goimports-0.29.0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "00daad2caf3c07d23e18121426d930be01e3129eb911d4094b27e9f13dff4a36"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ed7d6e51ecc7f95d04875b9b098c651c5c73337217d04db3b89ddd2f94a873b0"
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/goimports-0.30.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "dd7709abb6d24f0b9ecc6799afdd11e83ef67a6f57cb9e5062728ccb497c07f7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c53ce547dd8200a1f3a55901000aba85ba0d31e06d53cec56e97e7a28a65df51"
   end
 
   depends_on "go"
