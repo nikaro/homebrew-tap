e0dfa9cc9add716b02cf225d53432ac4fcb149ad
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2025-03-29 21:48:35 +0000

    claude-code: add 0.2.56 bottle.

--- Formula/claude-code.rb
+++ Formula/claude-code.rb
@@ -9,6 +9,12 @@ class ClaudeCode < Formula
   sha256 "9dc38922c67149baa3ac977bd608baf844c1bb82f1b6ca52f7905e0413e97996"
   license :cannot_represent
 
+  bottle do
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/claude-code-0.2.56"
+    sha256 cellar: :any,                 arm64_sonoma: "08f3b4bdb9108f003cdfbf78390ba36a90c0c481d0b6beb50671a2489537f7de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "220c4eaa87eab8b2d6b1592b85e20af149c9adf95f4bdefda455e70c397b2913"
+  end
+
   depends_on "node"
   depends_on "pcre2"
   depends_on "ripgrep"
