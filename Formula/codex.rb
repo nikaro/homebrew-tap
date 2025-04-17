904c12ec29834eda89fa3b22c590e85e8bb7db6f
Nicolas Karolak <nicolas@karolak.fr>
2025-04-17 20:39:22 +0200

    feat: add codex formula
    Closes #136.
    
    Signed-off-by: Nicolas Karolak <3918653+nikaro@users.noreply.github.com>

---
+++ Formula/codex.rb
@@ -0,0 +1,22 @@
+# typed: false
+
+require "language/node"
+
+class Codex < Formula
+  desc "Lightweight coding agent that runs in your terminal"
+  homepage "https://github.com/openai/codex"
+  url "https://registry.npmjs.org/@openai/codex/-/codex-0.1.2504161551.tgz"
+  sha256 "1036cd6ad6d6aa7490c8a8123704435af426efd48bebbe3f32539ac4b4c577a1"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/codex --version")
+  end
+end
