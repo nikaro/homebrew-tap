2f241376da40c244c38de4b40399a79a4bc38dfa
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2025-04-22 05:07:39 +0000

    codex: update 0.1.2504211509 bottle.

--- Formula/codex.rb
+++ Formula/codex.rb
@@ -10,9 +10,9 @@ class Codex < Formula
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/nikaro/homebrew-tap/releases/download/codex-0.1.2504172351"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "b09071d81d01d0444116475349b2ff0ff911a822384b816773c7ef137c614327"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "9daf6b638e8541fb2fe23623d5dbdb4cac221a101505c197b041fe3c053646ae"
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/codex-0.1.2504211509"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "51299359d637f2a752b72301e824a9d1596af461c54d4f0c57c70248c057e69e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0f30bc1912e13feee48c8f14d1db460b2ed06c78ed5393c4d59ae44e4e7ebf15"
   end
 
   depends_on "node"
