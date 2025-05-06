8f4fe2689d464ae6e4279927199d53d66b8e8d8c
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2025-05-06 05:02:37 +0000

    claude-code 0.2.102

--- Formula/claude-code.rb
+++ Formula/claude-code.rb
@@ -5,8 +5,8 @@ require "language/node"
 class ClaudeCode < Formula
   desc "Agentic coding tool that lives in your terminal"
   homepage "https://github.com/anthropics/claude-code"
-  url "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-0.2.100.tgz"
-  sha256 "702fbbe06278f023dc08bdf7a64911af1a4ac5a4e0f75e496b055e5c6f436839"
+  url "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-0.2.102.tgz"
+  sha256 "154816b39114459c5a59e414112b0264d10928bc3698d001d2bfc727ea57fd41"
   license :cannot_represent
 
   bottle do
