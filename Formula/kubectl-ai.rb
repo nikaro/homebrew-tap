d7f13652337bf3a19e41222008cf10bec44e1c45
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2025-05-08 08:52:15 +0000

    kubectl-ai: add 0.0.8 bottle.

--- Formula/kubectl-ai.rb
+++ Formula/kubectl-ai.rb
@@ -7,6 +7,12 @@ class KubectlAi < Formula
   sha256 "d4547d69a2b1014ed23989349eb210829373d43b94514896d2d4fecb6bb3488a"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/kubectl-ai-0.0.8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "d066b5548813867a4a5c10e84e201fe661798430b3e158f725507b0351dc4fe0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "fff251b1d83e45acde7474fb7161b25f362696ab3ea291ac75f14e90f90a0412"
+  end
+
   depends_on "go" => :build
 
   def install
