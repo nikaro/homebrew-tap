6a1d69412e9dea1c492878128c06efd9f5f1f267
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2025-06-12 20:51:47 +0000

    context7-mcp: add 1.0.13 bottle.

--- Formula/context7-mcp.rb
+++ Formula/context7-mcp.rb
@@ -9,6 +9,12 @@ class Context7Mcp < Formula
   sha256 "22e1ed19552cc8b7d8f7d9d37b46b55facee05d921b93300417a4ad669c89b24"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/nikaro/homebrew-tap/releases/download/context7-mcp-1.0.13"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "e13ef1a662a97cf7fd89f020be3a0e8e99c54dfd94a9bcbb1af365e5e661cbc7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1978a30838436b3e9de6ff9eb1c8d39c7856d5a589ae737553fcecbd339ce1fa"
+  end
+
   depends_on "node"
 
   def install
