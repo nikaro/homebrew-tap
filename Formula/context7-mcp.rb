f485ce9419998fe0d868661cf20eb4ce7a7480d7
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2025-06-14 05:02:45 +0000

    context7-mcp 1.0.14
    Closes #195.
    
    Signed-off-by: Nicolas Karolak <3918653+nikaro@users.noreply.github.com>

--- Formula/context7-mcp.rb
+++ Formula/context7-mcp.rb
@@ -5,8 +5,8 @@ require "language/node"
 class Context7Mcp < Formula
   desc "Up-to-date code documentation for LLMs and AI code editors"
   homepage "https://github.com/upstash/context7"
-  url "https://registry.npmjs.org/@upstash/context7-mcp/-/context7-mcp-1.0.13.tgz"
-  sha256 "22e1ed19552cc8b7d8f7d9d37b46b55facee05d921b93300417a4ad669c89b24"
+  url "https://registry.npmjs.org/@upstash/context7-mcp/-/context7-mcp-1.0.14.tgz"
+  sha256 "bb101a72e38224e176c93cdb3956f4ae396df02ff0bac7b6c68f444224fb08b0"
   license "MIT"
 
   bottle do
