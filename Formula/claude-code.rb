ff19bde4b32693e06f17f60d30eb21f58b035445
Nicolas Karolak <3918653+nikaro@users.noreply.github.com>
2025-06-14 05:02:33 +0000

    claude-code 1.0.24
    Closes #194.
    
    Signed-off-by: Nicolas Karolak <3918653+nikaro@users.noreply.github.com>

--- Formula/claude-code.rb
+++ Formula/claude-code.rb
@@ -5,8 +5,8 @@ require "language/node"
 class ClaudeCode < Formula
   desc "Agentic coding tool that lives in your terminal"
   homepage "https://github.com/anthropics/claude-code"
-  url "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-1.0.22.tgz"
-  sha256 "8261d65d17a01c62cf3d4e352fa3667f1d50e7bc80408efdfe6505b601bda816"
+  url "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-1.0.24.tgz"
+  sha256 "5573ebbc0ef233bb69678c006ec32d4cf2f94d1324365aa9ea29cb3e28a123b2"
   license :cannot_represent
 
   bottle do
