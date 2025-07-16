c8119dfc7b86874b0c5cb005b10bc7440beb8d9c
Nicolas Karolak <nicolas@karolak.fr>
2025-07-16 22:46:37 +0200

    feat: add tombi
    Closes #197.
    
    Signed-off-by: Nicolas Karolak <3918653+nikaro@users.noreply.github.com>

---
+++ Formula/tombi.rb
@@ -0,0 +1,32 @@
+# typed: false
+
+class Tombi < Formula
+  desc "TOML Formatter / Linter / Language Server"
+  homepage "https://github.com/tombi-toml/tombi"
+  url "https://github.com/tombi-toml/tombi/archive/refs/tags/v0.4.33.tar.gz"
+  sha256 "e0bd28b09e0f572863fb82a266f86fa1e5485807977c54ac285eacdab35e5baf"
+  license "MIT"
+  head "https://github.com/tombi-toml/tombi.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "rust/tombi-cli")
+  end
+
+  test do
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+    input = "Content-Length: #{json.size}\r\n\r\n#{json}"
+    pipe_output("#{bin}/tombi lsp", input)
+  end
+end
